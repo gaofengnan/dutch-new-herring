f4bbb9ac23ffe0a7dbfd1a4406a082fdfbe44c19
gaofengnan <gao.fengnan@gmail.com>
2022-11-11 23:50:49 +0800

    draw netherlands vendor map with population by municipality

--- ours/R-analyze-scripts/cbs.R
+++ ours/R-analyze-scripts/cbs.R
@@ -13,15 +13,17 @@ library(cbsodataR)
 # regional_key_figures <- cbsodata4::cbs4_get_data("70072ned")
 
 
-# pop <- read_delim("Population_dynamics__region_11112022_101336.csv", 
-#                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
-# # pop %>% rename("Population on 1 January (number)", "Population")
-# pop <- pop[pop$Periods =="2021",]
-# pop <- pop[!is.na(pop$`Population on 1 January (number)`),]
-# nrow(pop)
-# pop$Regions <- str_trim(pop$Regions)
+# tmp <- read_delim("dutch_municipality_2016_area.txt",delim = " ") 
+# print(tmp[,1])
+# 
+# write.csv(tmp[,1], file = 'area.csv',row.names=FALSE)
 
-library(tidyverse)
+pop_by_municipality_2016 <- read_excel("dutch_municipality_2016.xlsx")
+# as.numeric(pop_by_municipality_2016$Gemeentecode) - pop_by_municipality_2016$CBS_code
+pop_by_municipality_2016$statcode <- paste0("GM", pop_by_municipality_2016$Gemeentecode)
+
+
+# library(tidyverse)
 library(sf)
 
 
@@ -34,40 +36,42 @@ library(scico)
 # library(vapoRwave)
 # for data wrangling
 library(dplyr)
+ 
+  
+# Retrieve data with municipal boundaries from PDOK
+municipalBoundaries <- st_read("https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_gemeente_2017_gegeneraliseerd&outputFormat=json")
 
+statcode_2017 <- municipalBoundaries$statcode
+statcode_2016 <- pop_by_municipality_2016$statcode
+
+not_2017_idx <- statcode_2016 %in% statcode_2016[!(statcode_2016 %in% statcode_2017)]
+
+
+pop_density_2017 <- pop_by_municipality_2016[!not_2017_idx,]
+pop_density_2017 <- select(pop_density_2017, "Municipality_2017", "statcode", "Population_density")
+pop_density_2017 <- rbind(pop_density_2017, c("Meierijstad", "GM1948", 439))
+pop_density_2017$Population_density <- as.numeric(pop_density_2017$Population_density)
 
 # Find out which columns are available
 metadata <- cbs_get_meta("83765NED")
 print(metadata$DataProperties$Key)
 
-# Download birth rates and delete spaces from regional identifiers
-data <- cbs_get_data("83765NED", 
-                     select=c("WijkenEnBuurten","Gemeentenaam_1","GeboorteRelatief_25")) %>%
-                     # select=c("WijkenEnBuurten","GeboorteRelatief_25")) %>%
-  mutate(WijkenEnBuurten = str_trim(WijkenEnBuurten),
-         births = GeboorteRelatief_25)
 
-# Retrieve data with municipal boundaries from PDOK
-municipalBoundaries <- st_read("https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_gemeente_2017_gegeneraliseerd&outputFormat=json")
-
-
-# Link data from Statistics Netherlands to geodata
-data <- 
+data_new <- 
   municipalBoundaries %>%
-  left_join(data, by=c(statcode="WijkenEnBuurten"))
-
-data$pop_density <- runif(nrow(data))
+  left_join(pop_density_2017, by=c(statcode="statcode"))
 
 vendor_plot_title <- "Venders of 2016 and 2017, Dutch New Herring" # (with pop. density)"
 # Create a thematic map
-data %>%
+data_new %>%
   ggplot() +
-  geom_sf(aes(fill = pop_density)) +
+  geom_sf(aes(fill = Population_density)) +
   # geom_sf() +
-  scale_fill_viridis_c("pop. density\nby municipality") + 
+  # scale_fill_viridis_c("pop. density\nby municipality") + 
   scale_fill_continuous("pop. density\nby municipality",low = "white", high = "black", guide = "colorbar")+
-  geom_sf(color = "red", alpha = 0.5, size = 2,fill = "darkred", shape = 20, 
+  geom_sf(aes(shape=year,color=year), alpha = 0.65, size = 1.7, # shape = 20, 
           data = vendors_loc_sf) +
+  # scale_shape_manual(values = c(15, 16)) +
   # geom_point(data = ours.maps.df, aes(x = lon, y = lat), size = 4, 
   #            shape = 23, fill = "darkred")+
   labs(title = vendor_plot_title, fill = "") +
